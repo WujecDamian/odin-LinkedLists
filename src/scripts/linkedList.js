68c26b12fe40372226a990ccf6c751f9bba44b99
WujecDamian <wujecdamian12@gmail.com>
2026-03-19 03:02:04 +0100

    refactor: remove stolen code

--- src/scripts/linkedList.js
+++ src/scripts/linkedList.js
@@ -5,51 +5,15 @@ export function linkedList () {
     size: null,
     head: null,
     tail: null,
-    append (value) {
-      let newnode = Node(value)
-      if (this.head === null) {
-        this.head = newnode
-        return
-      }
-      let current = this.head
-      while (current.next != null) {
-        current = current.next
-      }
-      current.next = newnode
-    },
+    append (value) {},
     prepend (value) {},
-    size () {
-      let size = 1
-      if (this.head === null) {
-        return 0
-      }
-      let current = this.head
-      while (current.next != null) {
-        current = current.next
-        size++
-      }
-      return size
-    },
-    printHead () {
-      if (this.head === null) {
-        return undefined
-      }
-    },
+    size () {},
+    printHead () {},
     tail () {},
     at (index) {},
     pop () {},
     contains (value) {},
     findIndex (value) {},
-    toString () {
-      let result = ''
-      let current = this.head
-      result += `( ${current.value} ) -> `
-
-      while (current.next != null) {
-        current = current.next
-        result += `( ${current.value} ) -> `
-      }
-      return `${result}null`
-    }
+    toString () {}
   }
 }
